387fc60f67a9d8505205a40ba71196f9efab83ed
ProfessoraComputador <160857212+ProfessoraComputador@users.noreply.github.com>
2024-03-16 10:09:45 -0300

    Subindo arquivo do conteúdo: Tipos Primitivos.

---
+++ 1-tiposPrimitivos.js
@@ -0,0 +1,47 @@
+// Tipos Primitivos
+// Tipo Number
+
+const primeiroNumero1 = 45;
+const segundoNumero2 = 5;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica)
+
+// Tipo String
+
+// string com aspas duplas e simples
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+
+
+// concatenação (+)
+const citação = "Meu nome é ";
+const meuNome = "Liane";
+console.log(citação + meuNome);
+
+// Tipo boolean
+
+const primeiroNumero = 12;
+const segundoNumero = 25;
+
+console.log(primeiroNumero === segundoNumero);
+
+// Tipo Null e Undefined
+
+// undefined
+
+// null ==> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero)
+console.log(typeof texto)
